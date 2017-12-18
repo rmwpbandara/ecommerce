3e21a32ccc128d8edb40eab4e08e678782d19a04
Wasantha Padmasiri <rmwpbandara@gmail.com>
2017-12-18 10:56:07 +0530

    ghj

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,29 @@
 
-<p>jhgjf askjfhkafsaf</p>
+@foreach($stocks as $stock)
+    @extends('layouts.element')
 
+    @section('images')
+        <img class="image-img" src="images/{{$stock->user_id}}{{$stock->id}}frontImage.jpg">
+    @endsection
 
-@extends('layouts.element')
+    @section('product_type')
+        @foreach($types as $type)
+            @if($stock->type_id == $type->id)
+                <span>{{ $type->type }}</span>
+            @endif
+        @endforeach
+    @endsection
 
-<p>jhgjf after</p>
+    @section('product_name')
+        <span>{{ $stock->name}}</span>
+    @endsection
+
+    @section('product_price')
+        <span>Rs.{{ $stock->price}}/-</span>
+    @endsection
+
+    @section('cart')
+        <span>add to cart</span>
+    @endsection
+
+@endforeach
